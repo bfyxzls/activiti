57092bb5e7c2f44a8a6f1efb719687f4d23fe280
zhangzhanling <zhangzhanling@chinalawinfo.com>
2019-11-12 17:50:42 +0800

    add logger

--- src/main/java/com/lind/avtiviti/config/EsConfig.java
+++ src/main/java/com/lind/avtiviti/config/EsConfig.java
@@ -1,7 +1,5 @@
 package com.lind.avtiviti.config;
 
-import com.lind.avtiviti.es.EsBlog;
-import com.lind.avtiviti.es.EsBlogRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
@@ -9,6 +7,6 @@ import org.springframework.stereotype.Component;
 public class EsConfig {
     @Bean
     public User esBlog() {
-        return new User("test", "test", "test","hello");
+        return new User("test", "test", "test", "hello");
     }
 }
--- src/test/java/com/lind/avtiviti/BeanLoadTest.java
+++ src/test/java/com/lind/avtiviti/BeanLoadTest.java
@@ -2,16 +2,19 @@ package com.lind.avtiviti;
 
 import com.lind.avtiviti.config.User;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class BeanLoadTest {
+    static Logger logger= LoggerFactory.getLogger("BeanLoadTest");
     @Test
     public void loadEsBean() {
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
         applicationContext.scan("com.lind.avtiviti");
         applicationContext.refresh();
         User esBlog = (User) applicationContext.getBean("esBlog");
-        System.out.println(esBlog);
+        logger.info("info:{}",esBlog);
 
     }
 }
